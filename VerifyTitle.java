afdbcd9ac0ab3d16b343b6eb238b246ad62fbbd8
komaldawkhar <40751039+komaldawkhar@users.noreply.github.com>
2019-09-01 20:39:40 +0530

    Add files via upload
    Parallel Cross Browser Testing with Selenium using TestNG

---
+++ VerifyTitle.java
@@ -0,0 +1,44 @@
+package com.crossbrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class VerifyTitle 
+{
+	WebDriver driver;
+	
+	@Parameters("browser")
+	@Test
+	public void verifypageTitle(String browserName)
+	{
+		//Check for Firefox browser
+		if(browserName.equalsIgnoreCase("firefox"))
+		{
+			driver = new FirefoxDriver();
+		}
+		//Check for Chrome browser
+		else if(browserName.equalsIgnoreCase("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver","./chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+		//Check for Internet Explorer browser
+		else if(browserName.equalsIgnoreCase("IE"))
+		{
+			System.setProperty("webdriver.ie.driver", "./IEDriverServer.exe");
+           driver = new InternetExplorerDriver();			
+		}
+		
+		//To load the web page
+		driver.get("http://www.linkedin.com");
+		//Maximize the webpage
+		driver.manage().window().maximize();
+		//For getting title
+		System.out.println("Title: "+driver.getTitle());
+		
+	}
+}
